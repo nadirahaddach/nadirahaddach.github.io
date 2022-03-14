6a4e5a410652a0e272eec47f29f3f26ca1000f0f
nadira :D <89223568+nadirahaddach@users.noreply.github.com>
2022-03-13 21:45:59 -0700

    Create keypad.py

---
+++ python files/keypad.py
@@ -0,0 +1,12 @@
+#def keypad(keypad):
+ #   print("keypad!")
+ #   for i in range(len(keypad)):
+ #       for j in range(len(keypad[i])):
+ #           print(keypad[i][j], end="")
+#        print()
+
+def keypad():
+    print()
+    numbers = ['1 2 3','4 5 6','7 8 9','  0  ']
+    key_pad = '\n'.join(numbers)
+    print(key_pad)
