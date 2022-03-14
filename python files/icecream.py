a334518be55c16bed23e1ac5561a55df28e34635
nadira :D <89223568+nadirahaddach@users.noreply.github.com>
2022-03-13 22:17:51 -0700

    Create icecream.py

---
+++ python files/icecream.py
@@ -0,0 +1,62 @@
+import time
+import os
+
+Color34 = "\u001b[34m"
+Color37 = "\u001b[37m"
+
+def icecream1():
+    print("         _.-.")
+    print("       ,'/ //\ ")
+    print("      /// // /)")
+    print("     /// // //|")
+    print("    /// // ///")
+    print("   /// // ///")
+    print("  (`: // ///")
+    print("   `;`: ///")
+    print("   / /:`:/")
+    print("  / /  `'") 
+    print(" / /")
+    print("(_/")
+
+def icecream2():
+    print("              _.-.")
+    print("            ,'/ //\ ")
+    print("           /// // /)")
+    print("          /// // //|")
+    print("         /// // ///")
+    print("        /// // ///")
+    print("       (`: // ///")
+    print("        `;`: ///")
+    print("        / /:`:/")
+    print("       / /  `'") 
+    print("      / /")
+    print("     (_/")
+
+def icecream3():
+    print("                   _.-.")
+    print("                 ,'/ //\ ")
+    print("                /// // /)")
+    print("               /// // //|")
+    print("              /// // ///")
+    print("             /// // ///")
+    print("            (`: // ///")
+    print("             `;`: ///")
+    print("             / /:`:/")
+    print("            / /  `'") 
+    print("           / /")
+    print("          (_/")
+
+
+
+
+os.system("clear")
+time.sleep(.1)
+icecream1()
+time.sleep(.5)
+os.system("clear")
+icecream2()
+time.sleep(.5)
+os.system("clear")
+icecream3()
+time.sleep(.5)
+os.system("clear")
